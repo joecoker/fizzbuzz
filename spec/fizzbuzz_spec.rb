94f918989fcc35093618bfcaf659dd65392e83f6
joecoker <joe_coker@hotmail.co.uk>
2018-10-23 11:29:11 +0100

    Test for buzz

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,14 +1,12 @@
-<<<<<<< HEAD
+
 require "./lib/fizzbuzz"
 
-=======
->>>>>>> dc354a963b5133dde2bbd45f6b6901b230b67aae
 describe 'fizzbuzz' do
   it 'returns "fizz" when passed 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
-<<<<<<< HEAD
+  it 'returns "buzz" when passed 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
+
 end
-=======
-end
->>>>>>> dc354a963b5133dde2bbd45f6b6901b230b67aae
